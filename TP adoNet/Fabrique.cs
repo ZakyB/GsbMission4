43c637c2294bd140a283baf7c4d9bc952f4ed0d1
BENRABAH Brahim Zaky <64563717+ZakyB@users.noreply.github.com>
2021-03-22 21:41:55 +0100

    pouet

--- TP adoNet/Fabrique.cs
+++ TP adoNet/Fabrique.cs
@@ -14,7 +14,7 @@ namespace TP_adoNet
 
         private static string uidMysql = "root";
 
-        private static string mdpMysql = "";
+        private static string mdpMysql = "root";
 
         public static string ProviderMysql { get => providerMysql; }
         public static string DataBaseMysql { get => dataBaseMysql; }
